6ac3216506865f9a797ed1ed6d69b5b46dcd431b
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2025-08-22 08:31:59 +0200

    Updated postprocessor

--- customize_postprocessor.py
+++ customize_postprocessor.py
@@ -1,5 +1,8 @@
 from deoxys.experiment.postprocessor import SegmentationPostProcessor, \
     H5CalculateFScore, H5CalculateRecall, H5CalculatePrecision, H5CalculateFPR
+from deoxys.experiment.pipeline import SegmentationExperimentPipeline
+import os
+from time import time
 
 
 class MultiLabelH5CalculateFScore(H5CalculateFScore):
@@ -94,3 +97,34 @@ class MultiLabelSegmentationPostProcessor(SegmentationPostProcessor):
         'precision': MultiLabelH5CalculatePrecision,
         'FPR': MultiLabelH5CalculateFPR
     }
+
+    def calculate_metrics_single_3d(self, metrics=None, metrics_kwargs=None):
+        self.calculate_metrics_single(metrics, metrics_kwargs)
+        if not self.run_test:
+            map_folder = self.log_base_path + self.SINGLE_MAP_PATH
+
+            main_log_folder = self.log_base_path + self.MAP_PATH
+            try:
+                os.rename(map_folder, main_log_folder)
+            except Exception as e:
+                print("Files exist:", e)
+                print("Copying new logs file")
+                os.rename(main_log_folder,
+                          main_log_folder + '-' + str(time()))
+                os.rename(map_folder, main_log_folder)
+
+        else:
+            test_folder = self.log_base_path + self.TEST_OUTPUT_PATH
+            map_filename = test_folder + self.TEST_SINGLE_MAP_NAME
+
+            main_result_file_name = test_folder + self.TEST_MAP_NAME
+            try:
+                os.rename(map_filename, main_result_file_name)
+            except Exception as e:
+                print("Files exist:", e)
+                print("Copying new result file")
+                os.rename(main_result_file_name,
+                          main_result_file_name + '-' + str(time()) + '.csv')
+                os.rename(map_filename, main_result_file_name)
+
+        return self
